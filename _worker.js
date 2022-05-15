1ed0996aa33188ebd33d91d342379e5f568f4776
lickceo <71675662+lickceo@users.noreply.github.com>
2022-05-16 07:59:01 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
     async fetch(request, env) {
       let url = new URL(request.url);
       if (url.pathname.startsWith('/')) {
-        url.hostname="cloud.aijrc.com";
+        url.hostname="pan.aijrc.com";
         let new_request=new Request(url,request);
         return fetch(new_request);
       }
