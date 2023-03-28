925bae978d176472f423c67351752620a5f45b06
lickceo <71675662+lickceo@users.noreply.github.com>
2023-03-28 21:07:23 +0800

    Create _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
     async fetch(request, env) {
       let url = new URL(request.url);
       if (url.pathname.startsWith('/')) {
-        url.hostname="p.jrc.ink";
+        url.hostname="tv.jrc.ink";
         let new_request=new Request(url,request);
         return fetch(new_request);
       }
